8471db7b17594f3f1f06c98a02ffb75bf37852dc
Milton <deymilton85@gmail.com>
2024-03-30 15:56:16 +0600

    16th commit

--- src/components/AllProject.jsx
+++ src/components/AllProject.jsx
@@ -1,14 +1,23 @@
 import React, { Fragment } from 'react'
 import '../assets/css/custom.css'
 import '../assets/css/responsive.css'
-import project1 from '../assets/img/project-1.png'
-import project2 from '../assets/img/project-2.png'
-import project3 from '../assets/img/project-3.png'
-import project4 from '../assets/img/project-4.png'
-import project5 from '../assets/img/project-5.png'
-import project6 from '../assets/img/project-6.png'
-import project7 from '../assets/img/project-7.png'
-import project8 from '../assets/img/project-8.png'
+import img1 from '../assets/img/img-1.png'
+import img2 from '../assets/img/img-2.png'
+import img3 from '../assets/img/img-3.png'
+import img4 from '../assets/img/img-4.png'
+import img5 from '../assets/img/img-5.png'
+import img6 from '../assets/img/img-6.png'
+import img7 from '../assets/img/img-7.png'
+import img8 from '../assets/img/img-8.png'
+import img9 from '../assets/img/img-9.png'
+import img10 from '../assets/img/img-10.png'
+import img11 from '../assets/img/img-11.png'
+import img12 from '../assets/img/img-12.png'
+// import project4 from '../assets/img/project-4.png'
+// import project5 from '../assets/img/project-5.png'
+// import project6 from '../assets/img/project-6.png'
+// import project7 from '../assets/img/project-7.png'
+// import project8 from '../assets/img/project-8.png'
 // import project9 from '../assets/img/project-9.png'
 // import project10 from '../assets/img/project-10.png'
 // import project11 from '../assets/img/project-11.png'
@@ -45,7 +54,7 @@ const AllProject = () => {
                         <div className="project-slider-box">
                             <a href="project-detail.html" className="overlay-link"></a>
                             <div className="project-img">
-                                <img src={project1} alt="Project"/>
+                                <img src={img1} alt="Project"/>
                                 <span className="badge-year">
                                     <span className="badge-inner">
                                         <span>2024</span>
@@ -67,7 +76,7 @@ const AllProject = () => {
                         <div className="project-slider-box">
                             <a href="project-detail.html" className="overlay-link"></a>
                             <div className="project-img">
-                                <img src={project1} alt="Project"/>
+                                <img src={img2} alt="Project"/>
                                 <span className="badge-year">
                                     <span className="badge-inner">
                                         <span>2024</span>
@@ -89,7 +98,7 @@ const AllProject = () => {
                         <div className="project-slider-box">
                             <a href="project-detail.html" className="overlay-link"></a>
                             <div className="project-img">
-                                <img src={project2} alt="Project"/>
+                                <img src={img3} alt="Project"/>
                                 <span className="badge-year">
                                     <span className="badge-inner">
                                         <span>2024</span>
@@ -111,7 +120,7 @@ const AllProject = () => {
                         <div className="project-slider-box">
                             <a href="project-detail.html" className="overlay-link"></a>
                             <div className="project-img">
-                                <img src={project3} alt="Project"/>
+                                <img src={img4} alt="Project"/>
                                 <span className="badge-year">
                                     <span className="badge-inner">
                                         <span>2024</span>
@@ -133,7 +142,7 @@ const AllProject = () => {
                         <div className="project-slider-box">
                             <a href="project-detail.html" className="overlay-link"></a>
                             <div className="project-img">
-                                <img src={project4} alt="Project"/>
+                                <img src={img5} alt="Project"/>
                                 <span className="badge-year">
                                     <span className="badge-inner">
                                         <span>2024</span>
@@ -155,7 +164,7 @@ const AllProject = () => {
                         <div className="project-slider-box">
                             <a href="project-detail.html" className="overlay-link"></a>
                             <div className="project-img">
-                                <img src={project5} alt="Project"/>
+                                <img src={img6} alt="Project"/>
                                 <span className="badge-year">
                                     <span className="badge-inner">
                                         <span>2024</span>
@@ -177,7 +186,7 @@ const AllProject = () => {
                         <div className="project-slider-box">
                             <a href="project-detail.html" className="overlay-link"></a>
                             <div className="project-img">
-                                <img src={project6} alt="Project"/>
+                                <img src={img7} alt="Project"/>
                                 <span className="badge-year">
                                     <span className="badge-inner">
                                         <span>2024</span>
@@ -199,7 +208,7 @@ const AllProject = () => {
                         <div className="project-slider-box">
                             <a href="project-detail.html" className="overlay-link"></a>
                             <div className="project-img">
-                                <img src={project7} alt="Project"/>
+                                <img src={img8} alt="Project"/>
                                 <span className="badge-year">
                                     <span className="badge-inner">
                                         <span>2024</span>
@@ -210,6 +219,94 @@ const AllProject = () => {
                             <div className="project-content">
                                 <h3>Mystical Meodows</h3>
                                 <p>Web Development</p>
+                                <a href="project-detail.html" className="theme-btn common-box">
+                                    <span>
+                                        <span className="normal"><i className="las la-angle-right"></i></span>
+                                        <span className="hover"><img src="assets/imgs/right-arrow.svg" alt="icon"/></span>
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+                        <div className="project-slider-box">
+                            <a href="project-detail.html" className="overlay-link"></a>
+                            <div className="project-img">
+                                <img src={img9} alt="Project"/>
+                                <span className="badge-year">
+                                    <span className="badge-inner">
+                                        <span>2024</span>
+                                        <span>2024</span>
+                                    </span>
+                                </span>
+                            </div>
+                            <div className="project-content">
+                                <h3>Cybernetic Dreams</h3>
+                                <p>Brand Marketing</p>
+                                <a href="project-detail.html" className="theme-btn common-box">
+                                    <span>
+                                        <span className="normal"><i className="las la-angle-right"></i></span>
+                                        <span className="hover"><img src="assets/imgs/right-arrow.svg" alt="icon"/></span>
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+                        <div className="project-slider-box">
+                            <a href="project-detail.html" className="overlay-link"></a>
+                            <div className="project-img">
+                                <img src={img10} alt="Project"/>
+                                <span className="badge-year">
+                                    <span className="badge-inner">
+                                        <span>2024</span>
+                                        <span>2024</span>
+                                    </span>
+                                </span>
+                            </div>
+                            <div className="project-content">
+                                <h3>Light Play</h3>
+                                <p>SEO</p>
+                                <a href="project-detail.html" className="theme-btn common-box">
+                                    <span>
+                                        <span className="normal"><i className="las la-angle-right"></i></span>
+                                        <span className="hover"><img src="assets/imgs/right-arrow.svg" alt="icon"/></span>
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+                        <div className="project-slider-box">
+                            <a href="project-detail.html" className="overlay-link"></a>
+                            <div className="project-img">
+                                <img src={img11} alt="Project"/>
+                                <span className="badge-year">
+                                    <span className="badge-inner">
+                                        <span>2024</span>
+                                        <span>2024</span>
+                                    </span>
+                                </span>
+                            </div>
+                            <div className="project-content">
+                                <h3>Synthwave Sympony</h3>
+                                <p>Social Media</p>
+                                <a href="project-detail.html" className="theme-btn common-box">
+                                    <span>
+                                        <span className="normal"><i className="las la-angle-right"></i></span>
+                                        <span className="hover"><img src="assets/imgs/right-arrow.svg" alt="icon"/></span>
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+                        <div className="project-slider-box">
+                            <a href="project-detail.html" className="overlay-link"></a>
+                            <div className="project-img">
+                                <img src={img12} alt="Project"/>
+                                <span className="badge-year">
+                                    <span className="badge-inner">
+                                        <span>2024</span>
+                                        <span>2024</span>
+                                    </span>
+                                </span>
+                            </div>
+                            <div className="project-content">
+                                <h3>Mindscapes</h3>
+                                <p>Robotic Automation</p>
                                 <a href="project-detail.html" className="theme-btn common-box">
                                     <span>
                                         <span className="normal"><i className="las la-angle-right"></i></span>
@@ -219,94 +316,6 @@ const AllProject = () => {
                             </div>
                         </div>
                         {/* <div className="project-slider-box">
-                            <a href="project-detail.html" className="overlay-link"></a>
-                            <div className="project-img">
-                                <img src={project8} alt="Project"/>
-                                <span className="badge-year">
-                                    <span className="badge-inner">
-                                        <span>2024</span>
-                                        <span>2024</span>
-                                    </span>
-                                </span>
-                            </div>
-                            <div className="project-content">
-                                <h3>Cybernetic Dreams</h3>
-                                <p>Brand Marketing</p>
-                                <a href="project-detail.html" className="theme-btn common-box">
-                                    <span>
-                                        <span className="normal"><i className="las la-angle-right"></i></span>
-                                        <span className="hover"><img src="assets/imgs/right-arrow.svg" alt="icon"/></span>
-                                    </span>
-                                </a>
-                            </div>
-                        </div>
-                        <div className="project-slider-box">
-                            <a href="project-detail.html" className="overlay-link"></a>
-                            <div className="project-img">
-                                <img src={project10} alt="Project"/>
-                                <span className="badge-year">
-                                    <span className="badge-inner">
-                                        <span>2024</span>
-                                        <span>2024</span>
-                                    </span>
-                                </span>
-                            </div>
-                            <div className="project-content">
-                                <h3>Light Play</h3>
-                                <p>SEO</p>
-                                <a href="project-detail.html" className="theme-btn common-box">
-                                    <span>
-                                        <span className="normal"><i className="las la-angle-right"></i></span>
-                                        <span className="hover"><img src="assets/imgs/right-arrow.svg" alt="icon"/></span>
-                                    </span>
-                                </a>
-                            </div>
-                        </div>
-                        <div className="project-slider-box">
-                            <a href="project-detail.html" className="overlay-link"></a>
-                            <div className="project-img">
-                                <img src={project11} alt="Project"/>
-                                <span className="badge-year">
-                                    <span className="badge-inner">
-                                        <span>2024</span>
-                                        <span>2024</span>
-                                    </span>
-                                </span>
-                            </div>
-                            <div className="project-content">
-                                <h3>Synthwave Sympony</h3>
-                                <p>Social Media</p>
-                                <a href="project-detail.html" className="theme-btn common-box">
-                                    <span>
-                                        <span className="normal"><i className="las la-angle-right"></i></span>
-                                        <span className="hover"><img src="assets/imgs/right-arrow.svg" alt="icon"/></span>
-                                    </span>
-                                </a>
-                            </div>
-                        </div>
-                        <div className="project-slider-box">
-                            <a href="project-detail.html" className="overlay-link"></a>
-                            <div className="project-img">
-                                <img src={project12} alt="Project"/>
-                                <span className="badge-year">
-                                    <span className="badge-inner">
-                                        <span>2024</span>
-                                        <span>2024</span>
-                                    </span>
-                                </span>
-                            </div>
-                            <div className="project-content">
-                                <h3>Mindscapes</h3>
-                                <p>Robotic Automation</p>
-                                <a href="project-detail.html" className="theme-btn common-box">
-                                    <span>
-                                        <span className="normal"><i className="las la-angle-right"></i></span>
-                                        <span className="hover"><img src="assets/imgs/right-arrow.svg" alt="icon"/></span>
-                                    </span>
-                                </a>
-                            </div>
-                        </div>
-                        <div className="project-slider-box">
                             <a href="project-detail.html" className="overlay-link"></a>
                             <div className="project-img">
                                 <img src={project13} alt="Project"/>
